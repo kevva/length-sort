7a4a6918f8bdcae741602a26827e68853810b2a3
Kevin Martensson <kevinmartensson@gmail.com>
2015-02-23 20:21:49 +0100

    Tweaks

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 'use strict';
 
-/**
- * Sort an array of strings by length
- *
- * @param {String} a
- * @param {String} b
- * @api public
- */
-
 module.exports.desc = function (a, b) {
 	return b.length - a.length;
 };
